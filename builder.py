f80fca8236f0c918a5ed61f70f996caa58b19161
Xantos07 <coalstudio7@gmail.com>
2025-12-19 10:05:10 +0100

    feat: embedding

--- builder.py
+++ builder.py
@@ -1,13 +1,15 @@
 from chunking import chunking
+from embeddings import embed_documents
 
 def chat_bot_builder():
-    # 1er étape : découpage du texte en chunks
-    chunks = chunking()
+    # 1er étape : découpage du texte en chunks retournant les documents
+    documents = chunking()
 
     # 2eme étape : embedding des chunks
+    embeddings = embed_documents(documents)
 
     # 3eme étape : création de l'index FAISS
-
+    
     print(f"Création des données pour le chatbot terminée")
 
 
